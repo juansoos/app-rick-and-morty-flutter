3fc6558aa0b10cda3baf6922770faf1a2a31afc1
Julian Sotelo <juansoos@protonmail.com>
2023-09-24 09:51:15 -0500

    Change name of response model

--- lib/src/api/model/character_response.dart
+++ lib/src/api/model/character_response.dart
@@ -1,13 +1,13 @@
 import 'package:app_rick_and_morty_flutter/src/api/model/character.dart';
 import 'package:app_rick_and_morty_flutter/src/api/model/info.dart';
 
-class Response {
+class CharacterResponse {
   Info? info;
   List<Character>? results;
 
-  Response({this.info, this.results});
+  CharacterResponse({this.info, this.results});
 
-  Response.fromJson(Map<String, dynamic> json) {
+  CharacterResponse.fromJson(Map<String, dynamic> json) {
     info = json['info'] != null ? Info.fromJson(json['info']) : null;
     if (json['results'] != null) {
       results = <Character>[];
@@ -15,17 +15,5 @@ class Response {
         results!.add(Character.fromJson(v));
       });
     }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-
-    if (info != null) {
-      data['info'] = info!.toJson();
-    }
-    if (results != null) {
-      data['results'] = results!.map((v) => v.toJson()).toList();
-    }
-    return data;
   }
 }
--- lib/src/api/repository/character_repository.dart
+++ lib/src/api/repository/character_repository.dart
@@ -1,21 +1,19 @@
 import 'dart:convert';
 import 'package:app_rick_and_morty_flutter/src/api/http_client.dart';
-import 'package:app_rick_and_morty_flutter/src/api/model/response.dart';
+import 'package:app_rick_and_morty_flutter/src/api/model/character_response.dart';
 
 class CharacterRepository {
   CharacterRepository(this._service);
 
   final HttpClient _service;
 
-  get convert => null;
-
-  Future<Response?> getCharacters({int? page = 1}) async {
+  Future<CharacterResponse?> getCharacters({int? page = 1}) async {
     try {
       final path = '/character/?page=$page';
 
       final response = await _service.get(path);
 
-      return Response.fromJson(jsonDecode(response.body));
+      return CharacterResponse.fromJson(jsonDecode(response.body));
     } catch (e) {
       return null;
     }
